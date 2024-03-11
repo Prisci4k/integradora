e8e7de12092e576597cceca0e240b2870e4f859c
Prisci4k <angelrdzz023@gmail.com>
2024-03-11 15:16:15 -0600

    Agregado nuevo archivo y actualizaciones

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,19 @@
-// Import the functions you need from the SDKs you need
+// Importa las funciones necesarias del SDK de Firebase
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Tu configuración de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCZkSsnoY5D4PXJwJVfWuQDm_ddF-wJh68",
-  authDomain: "sireli-7595f.firebaseapp.com",
-  databaseURL: "https://sireli-7595f-default-rtdb.firebaseio.com",
-  projectId: "sireli-7595f",
-  storageBucket: "sireli-7595f.appspot.com",
-  messagingSenderId: "980490045809",
-  appId: "1:980490045809:web:e68e711d5ec00014b34ed0",
-  measurementId: "G-P4MNLG7SHM"
+  apiKey: "tu-api-key",
+  authDomain: "tu-auth-domain",
+  databaseURL: "tu-database-url",
+  projectId: "tu-project-id",
+  storageBucket: "tu-storage-bucket",
+  messagingSenderId: "tu-messaging-sender-id",
+  appId: "tu-app-id",
+  measurementId: "tu-measurement-id" // Opcional, solo si estás utilizando Google Analytics
 };
 
-// Initialize Firebase
+// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const analytics = getAnalytics(app); // Opcional, solo si estás utilizando Google Analytics
